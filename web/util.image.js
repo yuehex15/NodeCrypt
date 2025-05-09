ed3349cbf72c71c1ca9b1d973338bd30871ea65b
shuaiplus <2327005759@qq.com>
2025-05-09 22:56:35 +0800

    调整图片压缩质量，从0.95降低至0.90，以优化图片处理性能

--- web/util.image.js
+++ web/util.image.js
@@ -18,7 +18,7 @@ export async function processImage(file, callback) {
     const ctx = canvas.getContext('2d');
     ctx.drawImage(img, 0, 0, w, h);
     let dataUrl;
-    dataUrl = canvas.toDataURL('image/webp', 0.95);
+    dataUrl = canvas.toDataURL('image/webp', 0.90);
     callback(dataUrl);
   };
   const reader = new FileReader();
