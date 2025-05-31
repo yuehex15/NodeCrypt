dd6e0e787e3d64985cd29c4c91609851950d7abb
shuaiplus <2327005759@qq.com>
2025-05-31 13:49:46 +0800

    粗略的更新附件ui

--- client/js/chat.js
+++ client/js/chat.js
@@ -287,61 +287,56 @@ function renderFileMessage(fileData, isSender) {
 
 	// Check actual file transfer status
 	const transfer = window.fileTransfers ? window.fileTransfers.get(fileId) : null;
-	let statusText = 'Waiting...';
+	let statusText = '';
 	let progressWidth = '0%';
 	let downloadBtnStyle = 'display: none;';
+	let showProgress = false;
 	
 	if (transfer) {
 		if (transfer.status === 'sending') {
 			const progress = (transfer.sentVolumes / transfer.totalVolumes) * 100;
 			progressWidth = `${progress}%`;
 			statusText = `Sending ${transfer.sentVolumes}/${transfer.totalVolumes}`;
+			showProgress = true;
 		} else if (transfer.status === 'receiving') {
 			const progress = (transfer.receivedVolumes.size / transfer.totalVolumes) * 100;
 			progressWidth = `${progress}%`;
 			statusText = `Receiving ${transfer.receivedVolumes.size}/${transfer.totalVolumes}`;
+			showProgress = true;
 		} else if (transfer.status === 'completed') {
-			progressWidth = '100%';
-			statusText = '✓ Completed';
-			// 只有接收方才显示下载按钮
-			downloadBtnStyle = isSender ? 'display: none;' : 'display: inline-block;';
+			// 完成时不显示任何状态，只显示下载按钮
+			downloadBtnStyle = isSender ? 'display: none;' : 'display: flex;';
 		}
 	} else if (isSender) {
-		statusText = 'Sent';
-		progressWidth = '100%';
-		// 发送方不显示下载按钮
+		// 发送方历史消息，不显示状态和下载按钮
 		downloadBtnStyle = 'display: none;';
 	} else {
-		// 接收方历史消息
-		statusText = 'Ready to download';
-		progressWidth = '100%';
-		downloadBtnStyle = 'display: inline-block;';
+		// 接收方历史消息，直接显示下载按钮
+		downloadBtnStyle = 'display: flex;';
 	}
-
 	// Different icon for archives vs single files
 	const fileIcon = isArchive ? '📦' : '📄';
-	const downloadText = isArchive ? 'Download All' : 'Download';
 
 	return `
 		<div class="file-message" data-file-id="${fileId}">
-			<div class="file-info">
-				<div class="file-icon">${fileIcon}</div>
-				<div class="file-details">
-					<div class="file-name" title="${safeDisplayName}">${safeDisplayName}</div>
-					<div class="file-meta">
-						<span class="file-size">${displayMeta}</span>
+			<div class="file-main-content">
+				<div class="file-info">
+					<div class="file-icon">${fileIcon}</div>
+					<div class="file-details">
+						<div class="file-name" title="${safeDisplayName}">${safeDisplayName}</div>
+						<div class="file-meta">${displayMeta}</div>
 					</div>
 				</div>
+				<button class="file-download-btn" style="${downloadBtnStyle}" onclick="window.downloadFile('${fileId}')">
+					<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g stroke-width="0"></g><g stroke-linecap="round" stroke-linejoin="round"></g><g> <path fill-rule="evenodd" clip-rule="evenodd" d="M8 10C8 7.79086 9.79086 6 12 6C14.2091 6 16 7.79086 16 10V11H17C18.933 11 20.5 12.567 20.5 14.5C20.5 16.433 18.933 18 17 18H16.9C16.3477 18 15.9 18.4477 15.9 19C15.9 19.5523 16.3477 20 16.9 20H17C20.0376 20 22.5 17.5376 22.5 14.5C22.5 11.7793 20.5245 9.51997 17.9296 9.07824C17.4862 6.20213 15.0003 4 12 4C8.99974 4 6.51381 6.20213 6.07036 9.07824C3.47551 9.51997 1.5 11.7793 1.5 14.5C1.5 17.5376 3.96243 20 7 20H7.1C7.65228 20 8.1 19.5523 8.1 19C8.1 18.4477 7.65228 18 7.1 18H7C5.067 18 3.5 16.433 3.5 14.5C3.5 12.567 5.067 11 7 11H8V10ZM13 11C13 10.4477 12.5523 10 12 10C11.4477 10 11 10.4477 11 11V16.5858L9.70711 15.2929C9.31658 14.9024 8.68342 14.9024 8.29289 15.2929C7.90237 15.6834 7.90237 16.3166 8.29289 16.7071L11.2929 19.7071C11.6834 20.0976 12.3166 20.0976 12.7071 19.7071L15.7071 16.7071C16.0976 16.3166 16.0976 15.6834 15.7071 15.2929C15.3166 14.9024 14.6834 14.9024 14.2929 15.2929L13 16.5858V11Z" fill="currentColor"></path> </g></svg>
+				</button>
 			</div>
-			<div class="file-progress-container">
+			${showProgress ? `<div class="file-progress-container">
 				<div class="file-progress-bar">
 					<div class="file-progress" style="width: ${progressWidth}"></div>
 				</div>
 				<div class="file-status">${statusText}</div>
-			</div>
-			<button class="file-download-btn" style="${downloadBtnStyle}" onclick="window.downloadFile('${fileId}')">
-				📥 ${downloadText}
-			</button>
+			</div>` : ''}
 		</div>
 	`;
 }
